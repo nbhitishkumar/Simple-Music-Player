0690a744619443977a32e23e24d13c86077e9cda
Nitish Kumar <50609819+nbhitishkumar@users.noreply.github.com>
2019-09-30 18:05:01 +0530

    upload

---
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/Song.java
@@ -0,0 +1,32 @@
+package com.example.musicplayer;
+
+public class Song {
+    String path;
+    String album;
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    String title;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Simple-Music-Player
-A simple Music Player for playing songs
